8b4bf2cf9b32dbb1c8f536c4b7f68ae97ee16524
顾小五 <337118043@qq.com>
2017-08-30 10:48:43 +0800

    李振编写skill类

--- Skill.cpp
+++ Skill.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
 using namespace std;
+
+class Skill{
+public:
+    int damage(){       //返回伤害
+
+
+    }
+private:
+    string name;        //技能名字
+    int cost_mp;        //技能消耗mp
+    int cost_hp;        //技能消耗hp
+    int hurt_hp;        //技能初始伤害
+    int parameter;      //基础倍率
+};
